2bbc69e48144f921d33f58596d5274ce453f6d1b
schaererPanda <114098546+schaererPanda@users.noreply.github.com>
2023-08-06 22:15:15 -0500

    Change to using Railway MySQL server

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,4 +1,6 @@
 // ./database/db-connector.js
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Get an instance of mysql we can use in the app
 var mysql = require("mysql2/promise");
@@ -6,10 +8,11 @@ var mysql = require("mysql2/promise");
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "classmysql.engr.oregonstate.edu",
-  user: "cs340_clarkr2",
-  password: "Gigi123!",
-  database: "cs340_clarkr2",
+  host: "containers-us-west-35.railway.app",
+  user: "root",
+  password: process.env.MYSQL_PASSWORD,
+  database: "patient_physician_db",
+  port: 7487,
 });
 
 // Export it for use in our applicaiton
